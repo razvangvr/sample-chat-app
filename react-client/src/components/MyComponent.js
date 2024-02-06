525a4fc23db278f0c018ad60106ed21c8747db08
razvan.gaston <razvan.gaston@avios.com>
2024-02-07 01:10:02 +0200

    adding react FE

---
+++ react-client/src/components/MyComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = ()  => {
+    console.log("OnePropertyCmp");
+    return(
+        <div>
+            <h1>MyComponent</h1>
+        </div>
+    );
+}
+export default MyComponent;
--- src/main/java/com/example/graphql/PostGqlController.java
+++ src/main/java/com/example/graphql/PostGqlController.java
@@ -1,10 +1,13 @@
 package com.example.graphql;
 
 import org.reactivestreams.Publisher;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class PostGqlController {
@@ -14,6 +17,13 @@ public class PostGqlController {
     public Publisher<Post> onPostCreated(){
         return Flux.just(new Post("razvan_"+System.currentTimeMillis()));
     }
+
+    @QueryMapping("allPosts")
+    public List<Post> allPosts() {
+        return Collections.singletonList(new Post("meeeeA"));
+    }
+
+
 
 
 
