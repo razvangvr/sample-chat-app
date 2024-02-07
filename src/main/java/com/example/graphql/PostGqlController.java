a11b6136161e361dcb2d1f6189cdb40a1e3a4290
razvan.gaston <razvan.gaston@avios.com>
2024-02-07 09:49:32 +0200

    Emite Evenimente in Continuu

--- src/main/java/com/example/graphql/PostGqlController.java
+++ src/main/java/com/example/graphql/PostGqlController.java
@@ -6,8 +6,11 @@ import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class PostGqlController {
@@ -15,7 +18,18 @@ public class PostGqlController {
 
     @SubscriptionMapping("postCreated")
     public Publisher<Post> onPostCreated(){
-        return Flux.just(new Post("razvan_"+System.currentTimeMillis()));
+//        return Flux.just(new Post("razvan_"+System.currentTimeMillis()));
+
+        final AtomicInteger counter = new AtomicInteger();
+
+        List<Post> posts = new ArrayList<>();
+
+        for(int i = 0; i<=600; i++){
+            posts.add(new Post("razvan_"+i));
+        }
+
+        return Flux.fromIterable(posts)
+                .delayElements(Duration.ofSeconds(1));
     }
 
     @QueryMapping("allPosts")
